99dc79ed000b4f55c629269646edacba3ac6aeb3
Steper Kuo <gyx8899@126.com>
2020-05-19 00:26:06 +0800

    feature(single-number.js): added; chore(package.json/_index.js): updated;

--- site/leetcode/_index.js
+++ site/leetcode/_index.js
@@ -1,8 +1,10 @@
 import besttimetobuyandsellstock from './best-time-to-buy-and-sell-stock.js'
 import containsduplicate from './contains-duplicate.js'
 import longestsubstringwithoutrepeatingcharacters from './longest-substring-without-repeating-characters.js'
+import maximumnumberofballoons from './maximum-number-of-balloons.js'
 import maximumsubarray from './maximum-subarray.js'
 import productofarrayexceptself from './product-of-array-except-self.js'
+import singlenumber from './single-number.js'
 import threesum from './three-sum.js'
 import twosum from './two-sum.js'
 import validanagram from './valid-anagram.js'
@@ -12,8 +14,10 @@ export default {
 besttimetobuyandsellstock,
 containsduplicate,
 longestsubstringwithoutrepeatingcharacters,
+maximumnumberofballoons,
 maximumsubarray,
 productofarrayexceptself,
+singlenumber,
 threesum,
 twosum,
 validanagram,
---
+++ site/leetcode/single-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  let result = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    result = result ^ nums[i];
+  }
+
+  return result;
+};
